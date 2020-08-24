16b4acba9959cb4a62ffc08d24c898d86f4d3338
Builder212 <building2day@gmail.com>
2020-08-24 11:59:27 -0400

    edited sweep.py

--- servo/Sweep.py
+++ servo/Sweep.py
@@ -1,48 +1,34 @@
 import RPi.GPIO as GPIO
 import time
-OFFSE_DUTY = 0.5        #define pulse offset of servo
-SERVO_MIN_DUTY = 2.5+OFFSE_DUTY     #define pulse duty cycle for minimum angle of servo
-SERVO_MAX_DUTY = 12.5+OFFSE_DUTY    #define pulse duty cycle for maximum angle of servo
-servoPin = 12
 
-def map( value, fromLow, fromHigh, toLow, toHigh):  # map a value from one range to another range
-    return (toHigh-toLow)*(value-fromLow) / (fromHigh-fromLow) + toLow
+class servo:
+    def __init__(self, pin=12):
+        self.offset_duty = 0.5
+        self.min_duty = 2.5+self.offset_duty
+        self.max_duty = 12.5+self.offset_duty
+        self.pin = pin
 
-def setup():
-    global p
-    GPIO.setmode(GPIO.BOARD)         # use PHYSICAL GPIO Numbering
-    GPIO.setup(servoPin, GPIO.OUT)   # Set servoPin to OUTPUT mode
-    GPIO.output(servoPin, GPIO.LOW)  # Make servoPin output LOW level
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.pin, GPIO.OUT)
+        GPIO.output(self.pin, GPIO.LOW)
 
-    p = GPIO.PWM(servoPin, 50)     # set Frequece to 50Hz
-    p.start(0)                     # Set initial Duty Cycle to 0
+        p = GPIO.PWM(servoPin, 50)
+        p.start(0)
 
-def servoWrite(angle):      # make the servo rotate to specific angle, 0-180
-    if(angle<0):
-        angle = 0
-    elif(angle > 180):
-        angle = 180
-    p.ChangeDutyCycle(map(angle,0,180,0,SERVO_MAX_DUTY)) # map the angle to duty cycle and output it
+    def map(self, value, fromLow, fromHigh, toLow, toHigh):
+        return (toHigh-toLow)*(value-fromLow)/(fromHigh-fromLow)+toLow
 
-def loop():
-    while True:
-        for dc in range(0, 181, 1):   # make servo rotate from 0 to 180 deg
-            servoWrite(dc)     # Write dc value to servo
-            time.sleep(0.001)
-        time.sleep(0.5)
-        for dc in range(180, -1, -1): # make servo rotate from 180 to 0 deg
-            servoWrite(dc)
-            time.sleep(0.001)
-        time.sleep(0.5)
+    def servoWrite(self, angle):
+        if(angle<0):
+            angle = 0
+        elif(angle > 180):
+            angle = 180
+            p.ChangeDutyCycle(self.map(angle, 0, 180, self.min_duty, self.max_duty))
 
-def destroy():
-    p.stop()
-    GPIO.cleanup()
-
-if __name__ == '__main__':     # Program entrance
+if __name__ == '__main__':
     print ('Program is starting...')
-    setup()
+    servo = servo()
     try:
-        loop()
-    except KeyboardInterrupt:  # Press ctrl-c to end the program.
-        destroy()
+        servo.servoWrite(180)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
